2d4f2d4bd3678d330d6cf3556ff7c39c33610e8e
Katherine Perdue <krp0047@auburn.edu>
2023-02-04 17:02:14 -0500

    Change background image if AM or PM.

--- Timer Clock/ViewController.swift
+++ Timer Clock/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var clock: UILabel!
     var clockTimer = Timer()
     var clockFormat = DateFormatter()
+    var amPm = DateFormatter()
     
     
     
@@ -20,12 +22,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         clockFormat.dateFormat = "E, d MMM yyyy HH:mm:ss"
+        amPm.dateFormat = "a"
         clockTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.passTime), userInfo: nil, repeats: true)
         
     }
     
     @objc func passTime() {
         clock.text = clockFormat.string(from: Date())
+        if (amPm.string(from: Date()) == "AM") {
+            backgroundImage.image = UIImage(named: "dayBackground")
+            backgroundImage.alpha = 0.7
+        } else {
+            backgroundImage.image = UIImage(named: "nightBackground")
+            backgroundImage.alpha = 1.0
+        }
     }
 
 
