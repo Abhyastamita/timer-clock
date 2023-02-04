e267b48ab930d9390f796f264907139cbd76d5de
Katherine Perdue <krp0047@auburn.edu>
2023-02-04 17:45:02 -0500

    Timer counts down

--- Timer Clock/ViewController.swift
+++ Timer Clock/ViewController.swift
@@ -11,9 +11,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var clock: UILabel!
+    @IBOutlet weak var timerButton: UIButton!
+    @IBOutlet weak var timeRemainingLabel: UILabel!
+    @IBOutlet weak var timerPicker: UIDatePicker!
+    
     var clockTimer = Timer()
     var clockFormat = DateFormatter()
     var amPm = DateFormatter()
+    var timerFormat = DateComponentsFormatter()
+    var countDownTimer = Timer()
+    var timeLeft : TimeInterval?
     
     
     
@@ -23,7 +30,16 @@ class ViewController: UIViewController {
         
         clockFormat.dateFormat = "E, d MMM yyyy HH:mm:ss"
         amPm.dateFormat = "a"
+        // timerFormat.dateFormat = "HH:mm:ss"
         clockTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.passTime), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func startTimer(_ sender: UIButton) {
+        countDownTimer.invalidate()
+        timeLeft  = timerPicker.countDownDuration
+        
+        countDownTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startCountDown), userInfo: nil, repeats: true)
         
     }
     
@@ -35,6 +51,15 @@ class ViewController: UIViewController {
         } else {
             backgroundImage.image = UIImage(named: "nightBackground")
             backgroundImage.alpha = 1.0
+        }
+    }
+    
+    @objc func startCountDown() {
+        timeRemainingLabel.text = timerFormat.string(from: timeLeft!)
+        if timeLeft! >= 0 {
+            timeLeft! -= 1
+        } else {
+            countDownTimer.invalidate()
         }
     }
 
