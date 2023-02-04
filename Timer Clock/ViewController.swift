e15c7dad8d8bdafb4575b309880c391bb2da7372
Katherine Perdue <krp0047@auburn.edu>
2023-02-04 16:44:40 -0500

    Clock with correct formatting.

--- Timer Clock/ViewController.swift
+++ Timer Clock/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clock: UILabel!
+    var clockTimer = Timer()
+    var clockFormat = DateFormatter()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        clockFormat.dateFormat = "E, d MMM yyyy HH:mm:ss"
+        clockTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.passTime), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func passTime() {
+        clock.text = clockFormat.string(from: Date())
     }
 
 
